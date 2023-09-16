7b872ee4df99bc807940b7d5c70a6ead54f1eb76
jasim <jasjasim08@gmail.com>
2023-09-16 16:57:25 +0530

    password icon change

--- constant/index.js
+++ constant/index.js
@@ -1,6 +1,7 @@
 const IMAGES = {
   logo: "http://62.72.59.67:3200/images/MYV_ICONS.png",
   mailIcon: "http://62.72.59.67:3200/images/mailIcon.png",
+  passwordIcon: "http://62.72.59.67:3200/images/password.png",
 };
 
 module.exports = {
