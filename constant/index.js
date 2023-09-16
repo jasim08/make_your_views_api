6df4b4ec7a44f062f1904fdf2c1f85f262069592
jasim <jasjasim08@gmail.com>
2023-09-16 13:47:32 +0530

    push chanes

--- constant/index.js
+++ constant/index.js
@@ -1,6 +1,6 @@
 const IMAGES = {
-  logo: "https://62.72.59.67:3200/images/MYV_ICONS.png",
-  mailIcon: "https://62.72.59.67:3200/images/mailIcon.png",
+  logo: "http://62.72.59.67:3200/images/MYV_ICONS.png",
+  mailIcon: "http://62.72.59.67:3200/images/mailIcon.png",
 };
 
 module.exports = {
